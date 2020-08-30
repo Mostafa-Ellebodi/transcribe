d95efcf3ca05d7140ccb132f8257597d4f150f2a
Mostafa-Ellebodi <67235240+Mostafa-Ellebodi@users.noreply.github.com>
2020-08-30 13:46:53 +0200

    Update add_watermark.py

--- add_watermark.py
+++ add_watermark.py
@@ -1,23 +1,38 @@
+from pathlib import Path
 import moviepy.editor as mp
-import sys
 import os
+import shutil
 
-def add_logo(logo_dir, video_dir):
+def clean_mkdir(path):
+    if Path(path).exists():
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+out_videos = "converted_videos"
+clean_mkdir(out_videos)
+
+def convert_to_mp4_and_add_logo(logo_dir, video_dir):
     logo_image = str(os.listdir(logo_dir)[0])
-    video_file = os.listdir(video_dir)
+    video_names = os.listdir(video_dir)
 
-    for fn in video_file:
-      video = mp.VideoFileClip(video_dir+"/"+ fn)
+    for fn in video_names:
+        video = mp.VideoFileClip(video_dir+"/"+ fn)
 
-      logo = (mp.ImageClip(logo_dir+"/"+logo_image)
+        logo = (mp.ImageClip(logo_dir+"/"+logo_image)
                 .set_duration(video.duration)
                 .resize(height=50) # if you need to resize...
                 .margin(right=8, top=8, opacity=0) # (optional) logo-border padding
                 .set_pos(("right","top")))
 
-      final = mp.CompositeVideoClip([video, logo])
-      final.write_videofile("/content/transcribe/outputs_logo/"+fn)
-
-logo_dir = sys.argv[1]
-video_dir = sys.argv[2]
-add_logo(logo_dir, video_dir)
+        if fn.endswith('.mp4'):
+            final = mp.CompositeVideoClip([video, logo])
+            final.write_videofile(out_videos+ "/" +fn)
+            
+        else:
+            try:
+                fn_trim = os.path.splitext(fn)[0]
+                final = mp.CompositeVideoClip([video, logo])
+                final.write_videofile(out_videos+ "/" + fn_trim + ".mp4")
+            except:
+                print("Sorry format of file {} is not supported!" .format(fn) )
+                continue
